640b653cb97179109d502f75ad372662d0c9185f
Puri <purificacion.bastida@gmail.com>
2023-11-08 22:50:23 +0100

    Añadir ejer 3

---
+++ src/ejercicio-03/index.js
@@ -0,0 +1,12 @@
+// Creación de Objeto con Notación Literal
+
+// Utiliza la notación literal de objeto para crear un objeto llamado 
+// `book` con propiedades como `title`, `author` y `year`. Imprime el objeto en la consola.
+
+const book = {
+    title: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald',
+    year: 1925
+}
+console.log(book)
+
