3c758b5c498a6b284254d7f0d313e9aab48802d5
Puri <purificacion.bastida@gmail.com>
2023-11-08 23:12:23 +0100

    Añadir ejer 5

--- src/ejercicio-05/index.js
+++ src/ejercicio-05/index.js
@@ -1,1 +1,23 @@
-// Escribe tu código aquí:
+
+// Definiendo una Clase y Métodos
+// Crea una clase llamada `Rectangle` que tenga propiedades `width` 
+// y `height`. Define un método dentro de la clase que calcule el área
+// del rectángulo. Crea una instancia de la clase y muestra el área en la consola.
+
+class Rectangle {
+    constructor (width, height){
+        this.width = width
+        this.height = height
+    }
+
+    calculateArea() {
+        return this.width * this.height
+    }
+}
+
+const rectangle = new Rectangle(5, 10)
+
+console.log('Area:', rectangle.calculateArea())
+
+
+
