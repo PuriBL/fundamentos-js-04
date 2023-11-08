e6d0886ca932f39027651c097dcfebde10561c9e
Puri <purificacion.bastida@gmail.com>
2023-11-08 23:26:30 +0100

    Añadir ejer 6

--- src/ejercicio-06/index.js
+++ src/ejercicio-06/index.js
@@ -1,1 +1,28 @@
-// Escribe tu código aquí:
+
+// Herencia de Clases
+// Extiende la clase `Rectangle` para crear una clase `Square`
+// que también tenga un método para calcular el área, pero en este caso, 
+// el lado es el mismo para todas las direcciones. 
+// Crea una instancia de `Square` y muestra su área.
+
+class Rectangle {
+constructor (width, height) {
+    this.width = width
+    this.height = height
+}
+
+calculateArea() {
+    return this.width * this.height
+    }
+}
+class Square extends Rectangle {
+    constructor(sideLength) {
+        super(sideLength, sideLength)
+    }
+}
+const square = new Square(7)
+console.log('Area del cuadrado:', square.calculateArea())
+
+
+
+
