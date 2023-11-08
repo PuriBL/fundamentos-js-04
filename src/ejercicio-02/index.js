92247e7e528594d75c847515a07bf0c916096edc
Puri <purificacion.bastida@gmail.com>
2023-11-08 22:33:26 +0100

    Añadir ejer 2

--- src/ejercicio-02/index.js
+++ src/ejercicio-02/index.js
@@ -1,1 +1,21 @@
-// Escribe tu código aquí:
+
+// Recorriendo Propiedades con for...in
+
+// Define un objeto `car` con las propiedades `brand`, `model` y `year`. 
+// Asigna valores a estas propiedades y luego muestra la información completa 
+// el coche en la consola.
+
+const car = {
+    brand: 'Toyota',
+    model: 'Camry',
+    year: 2022    
+}
+
+for (let prop in car) {
+    console.log (`${prop}: ${car[prop]}`)  // No entiendo el símbolo del dólar
+}
+
+
+
+
+
