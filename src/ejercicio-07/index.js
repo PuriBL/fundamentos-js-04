209f4396dad455bbf8f7b24872764cf75426abd3
Puri <purificacion.bastida@gmail.com>
2023-11-09 20:25:37 +0100

    Añadir ejer 7

--- src/ejercicio-07/index.js
+++ src/ejercicio-07/index.js
@@ -1,1 +1,23 @@
-// Escribe tu código aquí:
+// Métodos Getters
+
+// Modifica la clase `Product` creada anteriormente para incluir
+// un método getter llamado `totalPrice` que calcule el precio total 
+// precio * cantidad). Luego, crea una instancia de `Product` y muestra 
+// su precio total.
+
+class Product {
+    constructor(name, price, quantity) {
+        this.name = name
+        this.price = price
+        this.quantity = quantity
+    }
+    get totalPrice() {
+        return this.price * this.quantity
+    }
+}
+
+const product = new Product('Shirt', 25.99, 3)
+
+console.log('Total Price shirt:', product.totalPrice)
+
+
