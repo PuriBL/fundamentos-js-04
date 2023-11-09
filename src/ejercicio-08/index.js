63c6639d5062040394cc55415e074fcc2204c9c6
Puri <purificacion.bastida@gmail.com>
2023-11-09 20:46:14 +0100

    Añadir ejer 8

--- src/ejercicio-08/index.js
+++ src/ejercicio-08/index.js
@@ -1,1 +1,27 @@
-// Escribe tu código aquí:
+// Agrega un método setter a la clase `Product` que permita modificar la cantidad 
+// del producto. Luego, utiliza el setter para cambiar la cantidad de una instancia
+// y muestra el precio total actualizado.
+
+class Product {
+    constructor(name, price, quantity) {
+        this.name = name
+        this.price = price
+        this._quantity = quantity
+    }
+    get quantity() {
+        return this._quantity
+    }
+
+    set quantity(newQuantity) {
+        this._quantity = newQuantity
+    }
+
+}
+const product = new Product('Hat', 19.99, 2)
+console.log('Total Price:', product.totalPrice) // 19.99 x 2
+
+product.quantity = 5
+console.log('Updated Total Price:', product.totalPrice) // 19.99 x 5
+
+
+
