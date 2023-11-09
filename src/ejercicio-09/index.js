f0c725a1c99a5f42bdb4a64234c1ad8be9584e58
Puri <purificacion.bastida@gmail.com>
2023-11-09 21:00:31 +0100

    Añadir ejer 9

--- src/ejercicio-09/index.js
+++ src/ejercicio-09/index.js
@@ -1,1 +1,17 @@
-// Escribe tu código aquí:
+// Uso de "this" en Clases:
+// Crea una clase `Person` con propiedades `name` y `age`. Agrega un método
+// `introduce` que muestre un mensaje como "Hola, soy [nombre] y tengo [edad] 
+// años". Crea una instancia de `Person` y llama al método `introduce`.
+
+class Person {
+    constructor (name, age) {
+        this.name = name
+        this.age = age
+    }
+    introduce() {
+        console.log(`Hola, soy ${this.name} y tengo ${this.age} años.`) // `` estas comillas      
+    }
+}
+const person = new Person('Puri', 28)
+person.introduce()
+
